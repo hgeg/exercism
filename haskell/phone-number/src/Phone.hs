6fe1ef6549e4158822183ec2d40f143d954376ba
Can Bulbul <can@orkestra.co>
2019-06-29 14:22:19 +0300

    completed extras of 3rd problem

--- haskell/phone-number/src/Phone.hs
+++ haskell/phone-number/src/Phone.hs
@@ -1,15 +1,19 @@
 module Phone (number) where
 
 import Data.Char (isDigit)
+import Control.Monad
 
 number :: String -> Maybe String
-number phone = consumeDigits . zip [isp..] digits
-  where digits = filter isDigit phone
-        isp    = if length digits == 10 then 1 else 0  
+number phone = case length digits of
+                 11 -> m1 d >> validate ds
+                 10 -> validate digits
+                 _  -> Nothing
+  where digits@(d:ds) = filter isDigit phone
+        validate = foldr (liftM2 (:)) (Just "") .
+                   zipWith ($) [m29, m09, m09, m29, m09, m09, m09, m09, m09, m09]
+        m1  c    = toMaybe "" $ c == '1'
+        m09 c    = toMaybe c  $ '0' <= c && c <= '9'
+        m29 c    = toMaybe c  $ '2' <= c && c <= '9'
 
-consumeDigits :: [(Integer, Char)] -> Maybe String
-consumeDigits ((0, d):ds) = is29
-consumeDigits ((1, d):ds) = 
-
-
-is29 c = c>='2' && c<='9'
+toMaybe :: a -> Bool -> Maybe a
+toMaybe v t = if t then Just v else Nothing
--- haskell/queen-attack/src/Queens.hs
+++ haskell/queen-attack/src/Queens.hs
@@ -1,7 +1,15 @@
 module Queens (boardString, canAttack) where
 
+import Data.List (intersperse)
+
 boardString :: Maybe (Int, Int) -> Maybe (Int, Int) -> String
-boardString white black = error "You need to implement this function."
+boardString white black = unlines [ intersperse ' ' [queenCheck (Just (r, c)) | c <- [0..7]] | r <- [0..7] ]
+  where queenCheck sq 
+          | sq == white = 'W'
+          | sq == black = 'B'
+          | otherwise  = '_'
 
 canAttack :: (Int, Int) -> (Int, Int) -> Bool
-canAttack queenA queenB = error "You need to implement this function."
+canAttack (rw, cw) (rb, cb) = rdiff==cdiff || rdiff==0 || cdiff==0
+  where rdiff = abs $ rw - rb
+        cdiff = abs $ cw - cb
